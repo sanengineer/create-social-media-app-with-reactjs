587cedeb93915e931efd33bb6401d583f40bd15e
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-01-17 12:16:30 +0700

    correct
    corection link be

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8000/api/v1", //local server
-  baseURL: "http://sosmetend.herokuapp.com/api/v1", //cloud xerver
+  baseURL: "https://sosmetend.herokuapp.com/api/v1", //cloud xerver
   headers: {
     "Content-type": "application/json",
   },
