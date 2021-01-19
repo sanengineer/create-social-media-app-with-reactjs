808041f5fbdc6339b6d105f32c13e99dbb8120a9
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-01-19 21:55:48 +0700

    update

--- src/components/cornerComponent.js
+++ src/components/cornerComponent.js
@@ -19,7 +19,7 @@ export const CornerComponent = ({ whoami, logOut, auth }) => {
     whoAmiProfileSrc = noPict;
   }
 
-  if (whoami == 0) {
+  if (whoami.length === 0) {
     return (
       <ul className="navbar-nav d-lg-flex order-4">
         <li className="...">
