8c84fe17da9685db4f03bd1bd1e8939a6567c138
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-01-31 06:43:49 +0700

    html parser

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1", //local server
-  // baseURL: "https://sosmetend.herokuapp.com/api/v1", //cloud xerver
+  // baseURL: "http://localhost:8000/api/v1", //local server
+  baseURL: "https://sosmetend.herokuapp.com/api/v1", //cloud xerver
   headers: {
     "Content-type": "application/json",
   },
 });
 
-const baseUrl = "http://localhost:8000/api/v1"; //local server
-// const baseUrl = "https://sosmetend.herokuapp.com/api/v1"; //cloud xerver
+// const baseUrl = "http://localhost:8000/api/v1"; //local server
+const baseUrl = "https://sosmetend.herokuapp.com/api/v1"; //cloud xerver
 
 export { api, baseUrl };
